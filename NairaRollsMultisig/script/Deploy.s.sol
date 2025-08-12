3ef0d8cfaae231d6230898543341fb7ab8cf63a6
pope-h <hamatthan@gmail.com>
2025-08-12 05:09:06 +0100

    feat: deployed factory

--- NairaRollsMultisig/script/Deploy.s.sol
+++ NairaRollsMultisig/script/Deploy.s.sol
@@ -1,19 +1,33 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import {Script} from "forge-std/Script.sol";
-// import {console} from "forge-std/console.sol";
-// import {NairaRollsMultisig} from "../src/NairaRollsMultisig.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {NairaRollsMultisigFactory} from "../src/NairaRollsMultisigFactory.sol";
 
-// contract DeployScript is Script {
-//     function run() external {
-//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-//         vm.startBroadcast(deployerPrivateKey);
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-//         Registry registry = new NairaRollsMultisig();
+        NairaRollsMultisigFactory nairaRollsMultisigFactory = new NairaRollsMultisigFactory();
 
-//         console.log("Registry deployed at:", address(registry));
+        console.log("NairaRolls Multisig Factory deployed at:", address(nairaRollsMultisigFactory));
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
+
+// forge script script/Deploy.s.sol --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvvv 
+
+// jq --version
+// brew install jq ===> if not installed
+
+// # Create directory first
+// mkdir -p extractedABIs
+
+// # Extract the ABI
+// jq '.abi' out/NairaRollsMultisig.sol/NairaRollsMultisig.json > extractedABIs/NairaRollsMultisig.json
+
+// # Also extract factory ABI
+// jq '.abi' out/NairaRollsMultisigFactory.sol/NairaRollsMultisigFactory.json > extractedABIs/NairaRollsMultisigFactory.json
