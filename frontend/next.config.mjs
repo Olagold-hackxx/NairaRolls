9bf09d5b0930368173b0eef646d58a7238e23c5a
pope-h <hamatthan@gmail.com>
2025-08-12 22:29:51 +0100

    feat: implement hook logic to approve and revoke batch

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BATCH_PAYROLL_MULTISIG_ABI: process.env.BATCH_PAYROLL_MULTISIG_ABI,
+    BATCH_PAYROLL_ADDRESS: process.env.BATCH_PAYROLL_ADDRESS,
     THIRDWEB_CLIENT_ID: process.env.THIRDWEB_CLIENT_ID,
     THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY,
   },
